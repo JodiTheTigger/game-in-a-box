c90a0a0f220645197ddadbbe65b3cbf8c02e7c87
Beautiful <jodi.the.tigger@gmail.com>
2012-11-28 21:10:17 +1300

    Added ReflectionManager unit test skeleton.

--- common/TestIReflected.cpp
+++ common/TestIReflected.cpp
@@ -18,7 +18,6 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "IReflected.h"
 #include "gtest/gtest.h"
 
 #include "IReflected.h"
--- common/TestReflectionManager.cpp
+++ common/TestReflectionManager.cpp
@@ -18,9 +18,122 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "ReflectionManager.h"
 #include "gtest/gtest.h"
+
+#include "IReflected.h"
+#include "ReflectionManager.h"
 
 using namespace std;
 
-// TODO!
+// I really should just use google mock.
+class ReflectedTester : public IReflected
+{
+    REFLECTION_BOILERPLATE(ReflectedTester)
+    
+public:
+    float FirstPropertyGet() const {return myFirst;}
+    void FirstPropertySet(float toSet) {myFirst = toSet;}
+    float SecondPropertyGet() const {return mySecond;}
+    void SecondPropertySet(float toSet) {mySecond = toSet;}
+    void ResetToZero() { myFirst = 0; mySecond = 0;}   
+    
+    virtual ~ReflectedTester() {}; 
+    
+private:
+    float myFirst = 1;
+    float mySecond = 2;        
+    
+    void InitReflection() override
+    {
+        REFLECTION_VARIABLE(ReflectedTester, FirstProperty);
+        REFLECTION_VARIABLE(ReflectedTester, SecondProperty);
+        REFLECTION_METHOD(ReflectedTester, ResetToZero);  
+    }   
+};
+
+class ReflectedTesterMethodsOnly : public IReflected
+{
+    REFLECTION_BOILERPLATE(ReflectedTesterMethodsOnly)
+    
+public:
+    void ResetToZero() { myFirst = 0; mySecond = 0;} 
+    void ResetToOne() { myFirst = 1; mySecond = 1;}
+    void UpdateValues(float& first, float& second) {first = myFirst; second = mySecond;}    
+    
+    virtual ~ReflectedTesterMethodsOnly() {}; 
+    
+private:
+    float myFirst = 1;
+    float mySecond = 2;        
+    
+    void InitReflection() override
+    {
+        REFLECTION_METHOD(ReflectedTesterMethodsOnly, ResetToZero);  
+        REFLECTION_METHOD(ReflectedTesterMethodsOnly, ResetToOne);  
+    }   
+};
+
+class TestReflectionManager : public ::testing::Test 
+{
+};
+
+
+TEST_F(TestReflectionManager, TestEmptyManager) 
+{
+  EXPECT_EQ(0, 1);
+}
+
+TEST_F(TestReflectionManager, TestAddOneClass) 
+{
+  EXPECT_EQ(0, 1);
+}
+
+TEST_F(TestReflectionManager, TestAddSameClassTwice) 
+{
+  EXPECT_EQ(0, 1);
+}
+
+TEST_F(TestReflectionManager, TestAddSameClassTwiceDifferentInstances) 
+{
+  EXPECT_EQ(0, 1);
+}
+
+TEST_F(TestReflectionManager, TestSetupDefaultsThenRegister) 
+{
+  EXPECT_EQ(0, 1);
+}
+
+TEST_F(TestReflectionManager, TestAddTwoClasses) 
+{
+  EXPECT_EQ(0, 1);
+}
+
+TEST_F(TestReflectionManager, TestSuggestionsEmptyString) 
+{
+  EXPECT_EQ(0, 1);
+}
+
+TEST_F(TestReflectionManager, TestSuggestionsOneChar) 
+{
+  EXPECT_EQ(0, 1);
+}
+
+TEST_F(TestReflectionManager, TestSuggestionsOneCharNoMatch) 
+{
+  EXPECT_EQ(0, 1);
+}
+
+TEST_F(TestReflectionManager, TestSuggestionsTwoChar) 
+{
+  EXPECT_EQ(0, 1);
+}
+
+TEST_F(TestReflectionManager, TestSuggestionsTwoCharNoMatch) 
+{
+  EXPECT_EQ(0, 1);
+}
+
+TEST_F(TestReflectionManager, TestSuggestionsOnlyOneMatch) 
+{
+  EXPECT_EQ(0, 1);
+}
