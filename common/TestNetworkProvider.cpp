285aa0ffc567c17ed480dc326eba04492eb7539f
Beautiful <jodi.the.tigger@gmail.com>
2013-01-07 06:52:54 +1300

    Added network provider unit test.

---
+++ common/TestNetworkProvider.cpp
@@ -0,0 +1,39 @@
+/*
+    Game-in-a-box. Simple First Person Shooter Network Game.
+    Copyright (C) 2012 Richard Maxwell <jodi.the.tigger@gmail.com>
+    
+    This file is part of Game-in-a-box
+
+    Game-in-a-box is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "NetworkProvider.h"
+#include "gtest/gtest.h"
+
+using namespace std;
+using namespace boost::asio::ip;
+
+// Class definition!
+class TestNetworkProvider : public ::testing::Test 
+{
+};
+
+TEST_F(TestNetworkProvider, GetPacketNullData) 
+{
+    NetworkProvider toTest;
+    udp::endpoint address;
+    unique_ptr<vector<uint8_t>> data;
+    
+    EXPECT_FALSE(toTest.GetPacket(address, data));  
+}
