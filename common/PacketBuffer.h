247b6c438450ec119c3ebdc8de560a39802e4111
Beautiful <jodi.the.tigger@gmail.com>
2013-02-22 07:17:53 +1300

    Added all command packets and test case skeleton. Doesn't compile.

---
+++ common/PacketBuffer.h
@@ -0,0 +1,59 @@
+/*
+    Game-in-a-box. Simple First Person Shooter Network Game.
+    Copyright (C) 2012 Richard Maxwell <jodi.the.tigger@gmail.com>
+
+    This file is part of Game-in-a-box
+
+    Game-in-a-box is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
+
+#ifndef PACKETBUFFER_H
+#define PACKETBUFFER_H
+
+#include "PacketCommand.h"
+
+template<PacketCommand::Command TheCommand>
+class PacketBuffer : public PacketCommand
+{
+public:
+    PacketBuffer() : PacketCommand(TheCommand) {}
+    virtual ~PacketBuffer() {}
+
+    virtual bool IsValid() const override
+    {
+        if (myBuffer.size() >= MinimumPacketSize)
+        {
+            return PacketCommand::IsValid();
+        }
+
+        return false;
+    }
+
+    std::vector<uint8_t> GetBuffer()
+    {
+        std::vector<uint8_t> result;
+
+        result.reserve(myBuffer.size() - OffsetPayload);
+
+        result.assign(myBuffer.begin() + OffsetPayload, myBuffer.end());
+
+        return result;
+    }
+
+protected:
+    static const std::size_t MinimumPacketSize = PacketCommand::MinimumPacketSize + 1;
+    static const std::size_t OffsetPayload = 3;
+};
+
+#endif // PACKETBUFFER_H
---
+++ common/TestPackets.cpp
@@ -0,0 +1,48 @@
+/*
+    Game-in-a-box. Simple First Person Shooter Network Game.
+    Copyright (C) 2012 Richard Maxwell <jodi.the.tigger@gmail.com>
+
+    This file is part of Game-in-a-box
+
+    Game-in-a-box is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "PacketSimple.h"
+#include "PacketChallenge.h"
+#include "PacketChallengeResponse.h"
+#include "gtest/gtest.h"
+
+using namespace std;
+
+// Class definition!
+class TestPacketCommand : public ::testing::Test
+{
+};
+
+TEST_F(TestPacketCommand, ChallengeFromEmptyData)
+{
+    PacketCommand::Command typeResult;
+
+    PacketChallenge challenge = PacketChallenge(vector<uint8_t>());
+
+    typeResult = challenge.GetCommand();
+
+    EXPECT_EQ(PacketCommand::Command::Invalid, typeResult);
+    EXPECT_FALSE(challenge.IsValid());
+}
+
+TEST_F(TestPacketCommand, TODO)
+{
+    EXPECT_FALSE(true);
+}
