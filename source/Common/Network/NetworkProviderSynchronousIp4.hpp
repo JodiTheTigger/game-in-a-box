83c271c544b8e8074d270d0a2f9943b21e7d2a6f
Beautiful <jodi.the.tigger@gmail.com>
2013-05-01 06:34:21 +1200

    Added stubbed boost asio ipv6 network provider.

---
+++ source/Common/Network/NetworkProviderSynchronousIp4.hpp
@@ -0,0 +1,56 @@
+/*
+    Game-in-a-box. Simple First Person Shooter Network Game.
+    Copyright (C) 2012-2013 Richard Maxwell <jodi.the.tigger@gmail.com>
+    
+    This file is part of Game-in-a-box
+    
+    Game-in-a-box is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+    
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
+
+#ifndef NETWORKPROVIDERSYNCHRONOUSIP4_H
+#define NETWORKPROVIDERSYNCHRONOUSIP4_H
+
+#include <boost/asio.hpp>
+#include "INetworkProvider.hpp"
+
+namespace GameInABox { namespace Common { namespace Network {
+class NetworkPacket;
+
+class NetworkProviderSynchronousIp4 final: public INetworkProvider
+{
+public:
+    NetworkProviderSynchronousIp4(boost::asio::ip::udp::endpoint bindAddress);
+    NetworkProviderSynchronousIp4()
+        : NetworkProviderSynchronousIp4(boost::asio::ip::udp::endpoint(boost::asio::ip::udp::v4(), 0))
+    {
+    }
+
+    // oh, I didn't know ~ctor = default; has a signature of
+    // ~ctor() noexcept(true) {}.
+    ~NetworkProviderSynchronousIp4() noexcept(true) override;
+
+private:
+    boost::asio::io_service myIoService;
+
+    std::vector<NetworkPacket> PrivateReceive() override;
+    void PrivateSend(std::vector<NetworkPacket> packets) override;
+    void PrivateReset() override;
+    void PrivateFlush() override;
+    void PrivateDisable() override;
+    bool PrivateIsDisabled() const override;
+};
+
+}}} // namespace
+
+#endif // NETWORKPROVIDERSYNCHRONOUSIP4_H
