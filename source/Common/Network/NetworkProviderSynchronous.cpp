a016368064370907d2f69b67c7c352b325cffa4d
Beautiful <jodi.the.tigger@gmail.com>
2013-05-04 17:35:58 +1200

    Fixed to pass more tests.

--- source/Common/Network/NetworkProviderSynchronous.cpp
+++ source/Common/Network/NetworkProviderSynchronous.cpp
@@ -63,6 +63,7 @@ std::vector<NetworkPacket> NetworkProviderSynchronous::PrivateReceive()
                 addressToUse);
 
             result.emplace_back(dataToUse, addressToUse);
+            available = mySocket->available();
         }
     }
 
