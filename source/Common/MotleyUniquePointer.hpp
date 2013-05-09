f1dc7e355e78f2693495bd69d6863de0be19a847
Beautiful <jodi.the.tigger@gmail.com>
2013-05-09 20:23:15 +1200

    unique_ptr enhancement that works with the NVI pattern. It stores a pointer to the original class, but exposes the base class so when it deletes the class it deletes the original class without the need of a virtual destructor.

---
+++ source/Common/MotleyUniquePointer.hpp
@@ -0,0 +1,111 @@
+/*
+    Game-in-a-box. Simple First Person Shooter Network Game.
+    Copyright (C) 2012-2013 Richard Maxwell <jodi.the.tigger@gmail.com>
+    
+    This file is part of Game-in-a-box
+    
+    Game-in-a-box is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+    
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
+
+#ifndef MOTLEYUNIQUEPOINTER_HPP
+#define MOTLEYUNIQUEPOINTER_HPP
+
+#include <memory>
+
+namespace GameInABox { namespace Common {
+
+// /////////////////////
+// Unique pointer that properly deletes a derived class
+// when stored as its base class.
+//
+// Based off:
+// http://stackoverflow.com/questions/8274451/well-how-does-the-custom-deleter-of-stdunique-ptr-work
+//
+// Interface at the bottom of the file
+//
+// /////////////////////
+
+// Anonymous namespace to keep it here
+namespace {
+
+template<typename DerivedType>
+void DeleteBaseClassAsDerived(void* baseClass)
+{
+    delete static_cast<DerivedType*>(baseClass);
+}
+
+class BoundDeleter
+{
+public:
+    template<typename DerivedType>
+    BoundDeleter(DerivedType* derived)
+        : myDeleter(&DeleteBaseClassAsDerived<DerivedType>)
+        , myDerivedObject(derived)
+    {}
+
+    BoundDeleter(BoundDeleter&& other)
+        : myDeleter(std::move(other.myDeleter))
+        , myDerivedObject(std::move(other.myDerivedObject))
+    {
+    }
+
+    void operator() (void*) const
+    {
+        myDeleter(myDerivedObject);
+    }
+
+private:
+    typedef void (*Deleter)(void*);
+
+    Deleter  myDeleter;
+    void*    myDerivedObject;
+
+};
+
+}
+
+// /////////////////////
+// Interface
+// /////////////////////
+template<typename BaseType>
+class MotleyUniquePointer : protected std::unique_ptr<BaseType, BoundDeleter>
+{
+public:
+    typedef std::unique_ptr<BaseType, BoundDeleter> Base;
+
+    using Base::operator->;
+    using Base::operator*;
+
+    template<typename ActualType>
+    MotleyUniquePointer(ActualType* derived)
+        : Base(derived, BoundDeleter(derived))
+    {
+    }
+
+    template<class Other>
+    MotleyUniquePointer(MotleyUniquePointer<Other>&& other)
+        : Base(std::move(other))
+    {
+    }
+};
+
+template<typename BaseType, typename DerivedType, typename... Args>
+MotleyUniquePointer<BaseType> make_motley_unique(Args&&... args)
+{
+    return MotleyUniquePointer<BaseType>(new DerivedType(std::forward<Args>(args)...));
+}
+
+}}
+
+#endif // MOTLEYUNIQUEPOINTER_HPP
