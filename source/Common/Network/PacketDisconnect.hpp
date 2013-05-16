9c782edf652eda23314eb0227a8fb4d08d68bde0
Richard Maxwell <jodi.the.tigger@gmail.com>
2013-05-17 08:18:39 +1200

    Added Specific Disconnect packet to prevent disconnect spoofing attacks.

---
+++ source/Common/Network/PacketDisconnect.hpp
@@ -0,0 +1,40 @@
+/*
+    Game-in-a-box. Simple First Person Shooter Network Game.
+    Copyright (C) 2012 Richard Maxwell <jodi.the.tigger@gmail.com>
+
+    This file is part of Game-in-a-box
+
+    Game-in-a-box is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
+
+#ifndef PACKETDISCONNECT_HPP
+#define PACKETDISCONNECT_HPP
+
+#ifndef USING_PRECOMPILED_HEADERS
+#include <string>
+#endif
+
+#include "PacketKey.hpp"
+
+namespace GameInABox { namespace Common { namespace Network {
+
+class PacketDisconnect : public PacketKey<Command::Disconnect>
+{
+public:
+    PacketDisconnect(uint32_t key, std::string failMessage);
+};
+
+}}} // namespace
+
+#endif // PACKETDISCONNECT_HPP
