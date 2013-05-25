fc6ac2ca20518f836bf2f15e81bb6b358b872972
Beautiful <jodi.the.tigger@gmail.com>
2013-05-25 20:57:54 +1200

    Added Connection unit test.

---
+++ test/Common/Network/TestConnection.cpp
@@ -0,0 +1,51 @@
+/*
+    Game-in-a-box. Simple First Person Shooter Network Game.
+    Copyright (C) 2012-2013 Richard Maxwell <jodi.the.tigger@gmail.com>
+    
+    This file is part of Game-in-a-box
+    
+    Game-in-a-box is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+    
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
+
+#include <cstdint>
+#include <chrono>
+#include <vector>
+#include <array>
+#include <gmock/gmock.h>
+
+#include <Common/Network/Connection.hpp>
+#include <Common/MockIStateManager.hpp>
+
+using namespace std;
+using Bytes = std::vector<uint8_t>;
+
+namespace GameInABox { namespace Common { namespace Network {
+
+// Class definition!
+class TestConnection : public ::testing::Test
+{
+public:
+    MockIStateManager mockState;
+};
+
+TEST_F(TestConnection, Create)
+{
+    Connection toTest(mockState);
+
+    EXPECT_FALSE(toTest.HasFailed());
+    EXPECT_FALSE(toTest.IsConnected());
+    EXPECT_FALSE(toTest.Handle());
+}
+
+}}} // namespace
