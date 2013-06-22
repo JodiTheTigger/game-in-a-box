88e8413a716788f2ac4cb04de93b6b0b8e2dd8fb
Beautiful <jodi.the.tigger@gmail.com>
2013-06-23 09:14:55 +1200

    Fixed #24: Added basic unit test for NetworkManagerServer.cpp

--- test/Common/Network/TestNetworkManagerServer.cpp
+++ test/Common/Network/TestNetworkManagerServer.cpp
@@ -1,36 +1,29 @@
 /*
     Game-in-a-box. Simple First Person Shooter Network Game.
     Copyright (C) 2012-2013 Richard Maxwell <jodi.the.tigger@gmail.com>
-    
+
     This file is part of Game-in-a-box
-    
+
     Game-in-a-box is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
-    
+
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU Affero General Public License for more details.
-    
+
     You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>
 */
 
 #include <gmock/gmock.h>
 
-#include <Common/Network/NetworkManagerClient.hpp>
+#include <Common/Network/NetworkManagerServer.hpp>
 #include <Common/Network/MockINetworkProvider.hpp>
 #include <Common/MockIStateManager.hpp>
 #include <Common/MotleyUniquePointer.hpp>
-
-// NOTE:
-// This only tests the class for valid state.
-// I'm not testing the process, or the data the mocks
-// are getting or setting.
-// I'm not testing timeouts due to the general rule of:
-// units test shouldn't take more than 250ms to complete.
 
 using namespace std;
 using Bytes = std::vector<uint8_t>;
@@ -41,51 +34,33 @@ using ::testing::AtLeast;
 namespace GameInABox { namespace Common { namespace Network {
 
 // Class definition!
-class TestNetworkManagerClient : public ::testing::Test
+class TestNetworkManagerServer : public ::testing::Test
 {
 public:
-    TestNetworkManagerClient()
+    TestNetworkManagerServer()
         : ipMock1(make_unique_motley<INetworkProvider, MockINetworkProvider>())
-        , ipMock2(make_unique_motley<INetworkProvider, MockINetworkProvider>())
         , stateMock()
-    {        
+    {
         // all equally probable.
         frequencies.fill(1);
     }
 
     MotleyUniquePointer<INetworkProvider> ipMock1;
-    MotleyUniquePointer<INetworkProvider> ipMock2;
     MockIStateManager stateMock;
     std::array<uint64_t, 256> frequencies;
 };
 
-TEST_F(TestNetworkManagerClient, CreateNoNet)
+
+TEST_F(TestNetworkManagerServer, CreateNet)
 {
     // right, what do we expect?
     EXPECT_CALL(stateMock, PrivateGetHuffmanFrequencies())
             .Times(AtLeast(1))
             .WillRepeatedly(Return(frequencies));
 
-    NetworkManagerClient toTest({}, stateMock);
-
-    EXPECT_FALSE(toTest.IsConnected());
-    EXPECT_FALSE(toTest.HasFailed());
-    EXPECT_EQ("", toTest.FailReason());
+    NetworkManagerServer(std::move(ipMock1), stateMock);
 }
 
-TEST_F(TestNetworkManagerClient, CreateTwoNet)
-{
-    std::vector<MotleyUniquePointer<INetworkProvider>> networks;
 
-    networks.push_back(std::move(ipMock1));
-    networks.push_back(std::move(ipMock2));
-
-    // right, what do we expect?
-    EXPECT_CALL(stateMock, PrivateGetHuffmanFrequencies())
-            .Times(AtLeast(1))
-            .WillRepeatedly(Return(frequencies));
-
-    NetworkManagerClient(std::move(networks), stateMock);
-}
 
 }}} // namespace
