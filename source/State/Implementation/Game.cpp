da915b12274a5eb0a7c2a6c798af0da07e24c1c3
Richard Maxwell <jodi.the.tigger@gmail.com>
2013-08-27 07:03:56 +1200

    Still a work in progress.

--- source/State/Implementation/Game.cpp
+++ source/State/Implementation/Game.cpp
@@ -25,6 +25,8 @@
 #include "DeltaMapItem.hpp"
 #include "Units.hpp"
 
+#include "PhysicsCube.hpp"
+
 #include <Common/MakeUnique.hpp>
 #include <vector>
 
@@ -89,55 +91,8 @@ Game::Game()
     // we have a network resolution of 20mm
     // the game is in a 1kmx1kmx1km box (+/- 1km = 1000m = 100,000cm = 50,000 network units = 17 bits).
 
-    /* try not to use the heap. leave here for now incase I do need to later.
-    std::array<std::unique_ptr<btCollisionShape>, 6> sidesP =
-    {
-        make_unique<btStaticPlaneShape>(btVector3(1,0,0),0),
-        make_unique<btStaticPlaneShape>(btVector3(-1,0,0),0),
-        make_unique<btStaticPlaneShape>(btVector3(0,1,0),0),
-        make_unique<btStaticPlaneShape>(btVector3(0,-1,0),0),
-        make_unique<btStaticPlaneShape>(btVector3(0,0,1),0),
-        make_unique<btStaticPlaneShape>(btVector3(0,0,-1),0)
-    };*/
-
-    std::array<btStaticPlaneShape, 6> sides =
-    {
-        btStaticPlaneShape{btVector3(1,0,0),0},
-        btStaticPlaneShape{btVector3(-1,0,0),0},
-        btStaticPlaneShape{btVector3(0,1,0),0},
-        btStaticPlaneShape{btVector3(0,-1,0),0},
-        btStaticPlaneShape{btVector3(0,0,1),0},
-        btStaticPlaneShape{btVector3(0,0,-1),0}
-    };
-
     auto player = btSphereShape{0.5};
     auto missle = btSphereShape{0.1};
-
-    std::array<btDefaultMotionState, 6> sideMotion =
-    {
-        btDefaultMotionState{btTransform(btQuaternion(0,0,0,1),btVector3(-500,0,0))},
-        btDefaultMotionState{btTransform(btQuaternion(0,0,0,1),btVector3(500,0,0))},
-        btDefaultMotionState{btTransform(btQuaternion(0,0,0,1),btVector3(0,-500,0))},
-        btDefaultMotionState{btTransform(btQuaternion(0,0,0,1),btVector3(0,500,0))},
-        btDefaultMotionState{btTransform(btQuaternion(0,0,0,1),btVector3(0,0,-500))},
-        btDefaultMotionState{btTransform(btQuaternion(0,0,0,1),btVector3(0,0,500))}
-    };
-
-    // let's do the walls.
-    for (auto i = 0; i < sides.size(); ++i)
-    {
-        btRigidBody::btRigidBodyConstructionInfo wallBody
-        (
-            0,
-            sideMotions[i],
-            sides[i],
-            btVector3(0,0,0)
-        );
-
-        // the ridid body needs to be dynamic.
-    }
-
-
 
     // person mass is 50kg, bullet mass is, um, 200g.
 }
