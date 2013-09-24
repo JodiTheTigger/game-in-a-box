ba6cb6137ec7b76bfca79b47908bcb49ad02044d
Richard Maxwell <jodi.the.tigger@gmail.com>
2013-09-25 08:19:41 +1200

    Added some player handling state.

---
+++ source/State/Implementation/ActPlayer.cpp
@@ -0,0 +1,61 @@
+/*
+    Game-in-a-box. Simple First Person Shooter Network Game.
+    Copyright (C) 2012-2013 Richard Maxwell <jodi.the.tigger@gmail.com>
+    
+    This file is part of Game-in-a-box
+    
+    Game-in-a-box is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+    
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
+
+#include "ActPlayer.hpp"
+#include "Flags.hpp"
+
+namespace GameInABox { namespace State { namespace Implementation {
+
+// Apply things like thrust and movement by setting velocity.
+
+bool CollidePlayer(const Entity& protagonist, const Entity& antagonist)
+{
+    return (&protagonist == &antagonist);
+}
+
+Entity ReactPlayer(Entity protagonist, const Entity&, const std::vector<const Entity*>&)
+{
+    auto result = protagonist;
+
+    // Jet latches on the direction you were facing when you start jetting.
+    if (FlagIsSet(result.player.input.action, FlagsPlayerAction::Jet))
+    {
+        if (!FlagIsSet(result.player.allowedAction, FlagsPlayerAction::Jet))
+        {
+            result.player.allowedAction = FlagSet(result.player.allowedAction, FlagsPlayerAction::Jet);
+            result.player.angleJet = result.player.input.angleLook;
+        }
+
+        // RAM: TODO: Add the actual thrust!
+    }
+    else
+    {
+        result.player.allowedAction = FlagClear(result.player.allowedAction, FlagsPlayerAction::Jet);
+    }
+
+    // RAM: TODO:
+    // Jet thrust
+    // Energy Regen
+    // Movement
+
+    return result;
+}
+
+}}} // namespace
--- source/State/Implementation/ActPlayer.hpp
+++ source/State/Implementation/ActPlayer.hpp
@@ -1,74 +1,51 @@
 /*
     Game-in-a-box. Simple First Person Shooter Network Game.
     Copyright (C) 2012-2013 Richard Maxwell <jodi.the.tigger@gmail.com>
-
+    
     This file is part of Game-in-a-box
-
+    
     Game-in-a-box is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
-
+    
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU Affero General Public License for more details.
-
+    
     You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>
 */
 
-#ifndef ENTITYPLAYER_HPP
-#define ENTITYPLAYER_HPP
+#ifndef ACTPLAYER_HPP
+#define ACTPLAYER_HPP
 
-#include "Types.hpp"
-#include "EntityTypes.hpp"
-
-#include <cstdint>
+#include "Interaction.hpp"
+#include "Factory.hpp"
+#include "Filters.hpp"
 
 namespace GameInABox { namespace State { namespace Implementation {
 
-enum class FlagsPlayerAction : uint32_t
+bool CollidePlayer(const Entity& protagonist, const Entity& antagonist);
+Entity ReactPlayer(Entity protagonist, const Entity&, const std::vector<const Entity*>& theWorld);
+
+template<>
+struct Factory<Interaction, EntityType::Time>
 {
-    None    = 0,
-    Foward  = (1 << 0),
-    Back    = (1 << 1),
-    Left    = (1 << 2),
-    Right   = (1 << 3),
-    Jet     = (1 << 4),
-    Fire    = (1 << 5)
-};
-
-struct PlayerAction
-{
-    FlagsPlayerAction action;
-    Vec3 angleLook;
-};
-
-struct EntityPlayer
-{
-    // RAM: TODO: Needed?
-    Id id;
-
-    // time is just another data point.
-    Tick time;
-
-    // Copied from the network system.
-    PlayerAction input;
-
-    Vec3 position;
-    Vec3 velocity;
-
-    Ammo energyShoot;
-
-    // jetting
-    Vec3 angleJet;
-
-    // Server side
-    Tick lastFired;
-    FlagsPlayerAction allowedAction;
+    static Interaction Get()
+    {
+        return
+        {
+            FilterPlayer,
+            FilterPlayer,
+            CollidePlayer,
+            ReactPlayer,
+            Interaction::Copy
+        };
+    }
 };
 
 }}} // namespace
 
-#endif // ENTITYPLAYER_HPP
+#endif // ACTPLAYER_HPP
