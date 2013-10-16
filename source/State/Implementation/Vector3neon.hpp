41f2ed5437e003f444c99ff0198a5e9f980eb459
Richard Maxwell <jodi.the.tigger@gmail.com>
2013-10-16 16:59:43 +1300

    Added cpp guts to sse2 and neon vector libraries.

--- source/State/Implementation/Vector3neon.hpp
+++ source/State/Implementation/Vector3neon.hpp
@@ -21,4 +21,402 @@
 #ifndef VECTOR3NEON_HPP
 #define VECTOR3NEON_HPP
 
+#include "Vector.hpp"
+
+#include <array>
+#include <cmath>
+#include <limits>
+
+// RAM: TODO: single inputs are called lhs, double, lhs and rhs. Validate.
+
+namespace GameInABox { namespace State { namespace Implementation {
+
+struct alignas(16) Vector3neon
+{
+    struct tagReplicate {};
+    struct tagAccurate {};
+
+    std::array<float, 4> values;
+
+    constexpr Vector3neon()
+        : values{{0.0f, 0.0f, 0.0f, 0.0f}} {}
+    constexpr Vector3neon(float x)
+        : values{{x, 0.0f, 0.0f, 0.0f}} {}
+    constexpr Vector3neon(float x, float y)
+        : values{{x, y, 0.0f, 0.0f}} {}
+    constexpr Vector3neon(float x, float y, float z)
+        : values{{x, y, z, 0.0f}} {}
+    constexpr Vector3neon(float x, float y, float z, float w)
+        : values{{x, y, z, w}} {}
+
+    constexpr Vector3neon(Vector vector)
+        : values(vector.values) {}
+    constexpr Vector3neon(const std::array<float, 4>& array)
+        : values(array) {}
+    constexpr Vector3neon(float x, tagReplicate)
+        : values{{x, x, x, x}} {}
+
+    Vector3neon(const Vector3neon&) = default;
+    Vector3neon(Vector3neon&&) = default;
+    Vector3neon& operator=(const Vector3neon&) & = default;
+    Vector3neon& operator=(Vector3neon&&) & = default;
+
+    constexpr Vector ToVector() const { return Vector{values}; }
+};
+
+
+// ///////////////////
+// Save
+// ///////////////////
+
+inline constexpr Vector Save(Vector3neon v)
+{
+    return Vector
+    {
+            v.values,
+    };
+}
+
+// ///////////////////
+// Operators
+// ///////////////////
+// Taken from http://stackoverflow.com/questions/4421706/operator-overloading/4421719
+// However all "inclass" operators changed to out of class.
+
+// ///////////////////
+// Increment / Decrement
+// ///////////////////
+inline Vector3neon& operator++(Vector3neon& rhs)
+{
+    ++(rhs.values[0]);
+    ++(rhs.values[1]);
+    ++(rhs.values[2]);
+
+    return rhs;
+}
+
+inline Vector3neon operator++(Vector3neon& lhs, int)
+{
+   auto copy = lhs;
+   ++lhs;
+
+   return copy;
+}
+
+inline Vector3neon& operator--(Vector3neon& rhs)
+{
+    --(rhs.values[0]);
+    --(rhs.values[1]);
+    --(rhs.values[2]);
+
+    return rhs;
+}
+
+inline Vector3neon operator--(Vector3neon& lhs, int)
+{
+    auto copy = lhs;
+    --lhs;
+
+    return copy;
+}
+
+// ///////////////////
+// Comparison Operators
+// ///////////////////
+inline bool operator==(const Vector3neon& lhs, const Vector3neon& rhs)
+{
+    return  (
+                (lhs.values[0]==rhs.values[0]) &&
+                (lhs.values[1]==rhs.values[1]) &&
+                (lhs.values[2]==rhs.values[2])
+            );
+}
+
+inline bool operator!=(const Vector3neon& lhs, const Vector3neon& rhs){return  !operator==(lhs,rhs);}
+
+// ///////////////////
+// Prototypes
+// ///////////////////
+inline Vector3neon Absolute(const Vector3neon& lhs);
+
+// ///////////////////
+// Simple Maths
+// ///////////////////
+inline Vector3neon& operator+=(Vector3neon& lhs, const Vector3neon& rhs)
+{
+    lhs.values[0] += rhs.values[0];
+    lhs.values[1] += rhs.values[1];
+    lhs.values[2] += rhs.values[2];
+    return lhs;
+}
+
+inline Vector3neon& operator-=(Vector3neon& lhs, const Vector3neon& rhs)
+{
+    lhs.values[0] -= rhs.values[0];
+    lhs.values[1] -= rhs.values[1];
+    lhs.values[2] -= rhs.values[2];
+    return lhs;
+}
+
+inline Vector3neon& operator*=(Vector3neon& lhs, const Vector3neon& rhs)
+{
+    lhs.values[0] *= rhs.values[0];
+    lhs.values[1] *= rhs.values[1];
+    lhs.values[2] *= rhs.values[2];
+    return lhs;
+}
+
+inline Vector3neon& operator/=(Vector3neon& lhs, const Vector3neon& rhs)
+{
+    lhs.values[0] /= rhs.values[0];
+    lhs.values[1] /= rhs.values[1];
+    lhs.values[2] /= rhs.values[2];
+    return lhs;
+}
+
+inline constexpr Vector3neon operator-(const Vector3neon& lhs)
+{
+    return Vector3neon
+    {
+        -lhs.values[0],
+        -lhs.values[1],
+        -lhs.values[2],
+        0.0f
+    };
+}
+
+inline Vector3neon operator+(Vector3neon lhs, const Vector3neon& rhs){ lhs += rhs;  return lhs; }
+inline Vector3neon operator-(Vector3neon lhs, const Vector3neon& rhs){ lhs -= rhs;  return lhs; }
+inline Vector3neon operator*(Vector3neon lhs, const Vector3neon& rhs){ lhs *= rhs;  return lhs; }
+inline Vector3neon operator/(Vector3neon lhs, const Vector3neon& rhs){ lhs /= rhs;  return lhs; }
+
+inline Vector3neon& operator*=(Vector3neon& lhs, float rhs)
+{
+    lhs.values[0] *= rhs;
+    lhs.values[1] *= rhs;
+    lhs.values[2] *= rhs;
+    return lhs;
+}
+
+inline Vector3neon& operator/=(Vector3neon& lhs, float rhs)
+{
+    return lhs *= 1.0f / rhs;
+}
+
+inline Vector3neon operator*(Vector3neon lhs, float rhs){ lhs *= rhs;  return lhs; }
+inline Vector3neon operator/(Vector3neon lhs, float rhs){ lhs /= rhs;  return lhs; }
+
+// ///////////////////
+// Complicated Maths (single return)
+// ///////////////////
+
+inline float DotF(const Vector3neon& lhs, const Vector3neon& rhs)
+{
+    return
+            (lhs.values[0] * rhs.values[0]) +
+            (lhs.values[1] * rhs.values[1]) +
+            (lhs.values[2] * rhs.values[2]) +
+            (lhs.values[3] * rhs.values[3]);
+}
+
+inline float LengthSquaredF(const Vector3neon& lhs)
+{
+    return DotF(lhs, lhs);
+}
+
+inline float LengthF(const Vector3neon& lhs)
+{
+    return sqrt(LengthSquaredF(lhs));
+}
+
+inline float DistanceF(const Vector3neon& lhs, const Vector3neon& rhs)
+{
+    return LengthF(lhs - rhs);
+}
+
+inline float DistanceSquaredF(const Vector3neon& lhs, const Vector3neon& rhs)
+{
+    return LengthSquaredF(lhs - rhs);
+}
+
+// RAM: TODO: Use Radians type.
+inline float Angle(const Vector3neon& lhs, const Vector3neon& rhs)
+{
+    auto squaredLengths = LengthSquaredF(lhs) * LengthSquaredF(rhs);
+
+    return acos(DotF(lhs, rhs) / squaredLengths);
+}
+
+// RAM: TODO: What does this do? Used to figure out the determinant of matrixes. Copied from btVector3
+inline float TripleF(const Vector3neon& lhs, const Vector3neon& v1, const Vector3neon& v2)
+{
+    return
+        lhs.values[0] * (v1.values[1] * v2.values[2] - v1.values[2] * v2.values[1]) +
+        lhs.values[1] * (v1.values[2] * v2.values[0] - v1.values[0] * v2.values[2]) +
+        lhs.values[2] * (v1.values[0] * v2.values[1] - v1.values[1] * v2.values[0]);
+}
+
+inline int AxisMax(const Vector3neon& lhs)
+{
+    return lhs.values[0] < lhs.values[1] ?
+            (lhs.values[1] < lhs.values[2] ? 2 : 1) :
+            (lhs.values[0] < lhs.values[2] ? 2 : 0);
+}
+
+inline int AxisMin(const Vector3neon& lhs)
+{
+    return lhs.values[0] < lhs.values[1] ?
+            (lhs.values[0] < lhs.values[2] ? 0 : 2) :
+            (lhs.values[1] < lhs.values[2] ? 1 : 2);
+}
+
+inline int AxisFar(const Vector3neon& lhs)
+{
+    return AxisMax(Absolute(lhs));
+}
+
+inline int AxisNear(const Vector3neon& lhs)
+{
+    return AxisMin(Absolute(lhs));
+}
+
+inline bool IsZero(const Vector3neon& lhs)
+{
+    return (lhs.values[0] == 0.0f) &&
+           (lhs.values[1] == 0.0f) &&
+           (lhs.values[2] == 0.0f);
+}
+
+inline bool IsZeroFuzzy(const Vector3neon& lhs)
+{
+    return LengthSquaredF(lhs) < std::numeric_limits<float>::epsilon();
+}
+
+// ///////////////////
+// Complicated Maths (vector return)
+// ///////////////////
+
+// RAM: TODO: vector versions of all of the above.
+// Need all the single return functions to support vector return too.
+// http://www.gamasutra.com/view/feature/132636/designing_fast_crossplatform_simd_.php?print=1
+// (Keep Results Into SIMD Registers)
+// That is, stop casting between SIMD and Float registers when it can all be kept in SIMD.
+//inline Vector3neon LengthSquared(const Vector3neon& lhs);
+//inline Vector3neon Length(const Vector3neon& lhs);
+//inline Vector3neon Distance(const Vector3neon& lhs, const Vector3neon& rhs);
+//inline Vector3neon DistanceSquared(const Vector3neon& lhs, const Vector3neon& rhs);
+//// This too?
+//inline Vector3neon Angle(const Vector3neon& lhs, const Vector3neon& rhs);
+
+inline Vector3neon Dot(const Vector3neon& lhs, const Vector3neon& rhs)
+{
+    float dot = DotF(lhs, rhs);
+
+    return Vector3neon
+    {
+        dot,
+        dot,
+        dot
+    };
+}
+
+inline Vector3neon Dot3(const Vector3neon &lhs, const Vector3neon &v0, const Vector3neon &v1, const Vector3neon &v2)
+{
+    return Vector3neon
+    {
+        DotF(lhs, v0),
+        DotF(lhs, v1),
+        DotF(lhs, v2)
+    };
+}
+
+inline Vector3neon Absolute(const Vector3neon& lhs)
+{
+    return Vector3neon
+    {
+        std::fabs(lhs.values[0]),
+        std::fabs(lhs.values[1]),
+        std::fabs(lhs.values[2]),
+        0.0f
+    };
+}
+
+inline Vector3neon NormaliseStable(Vector3neon lhs)
+{
+    auto absolute = Absolute(lhs);
+    auto maxIndex = AxisMax(absolute);
+
+    if (absolute.values[maxIndex] > 0)
+    {
+        lhs /= absolute.values[maxIndex];
+        return lhs / LengthF(lhs);
+    }
+
+    // Too small to actually normalise without becoming unstable.
+    // So just set x to 1. Same diff.
+    return Vector3neon
+    {
+            1.0f
+    };
+}
+
+inline Vector3neon Normalise(Vector3neon lhs)
+{
+    return lhs / LengthF(lhs);
+}
+
+inline Vector3neon Cross(const Vector3neon& lhs, const Vector3neon& rhs)
+{
+    return Vector3neon
+    {
+        lhs.values[1] * rhs.values[2] - lhs.values[2] * rhs.values[1],
+        lhs.values[2] * rhs.values[0] - lhs.values[0] * rhs.values[2],
+        lhs.values[0] * rhs.values[1] - lhs.values[1] * rhs.values[0]
+    };
+}
+
+// RAM: TODO: Use rotation in radians.
+inline Vector3neon Rotate(Vector3neon lhs, const Vector3neon& wAxis, float rotation)
+{
+    // Stole this from  bullet3's btVector3
+
+    auto o = wAxis * DotF(wAxis, lhs);
+    auto _x = lhs - o;
+    auto _y = Cross(wAxis, lhs);
+
+    return (o + _x * cosf(rotation) + _y * sinf(rotation));
+}
+
+inline Vector3neon Lerp(const Vector3neon& lhs, const Vector3neon& rhs, float scale)
+{
+    return Vector3neon
+    {
+        lhs.values[0] + (rhs.values[0] - lhs.values[0]) * scale,
+        lhs.values[1] + (rhs.values[1] - lhs.values[1]) * scale,
+        lhs.values[2] + (rhs.values[2] - lhs.values[2]) * scale
+    };
+}
+
+inline Vector3neon Max(const Vector3neon& lhs, const Vector3neon& rhs)
+{
+    return Vector3neon
+    {
+        lhs.values[0] > rhs.values[0] ? lhs.values[0] : rhs.values[0],
+        lhs.values[1] > rhs.values[1] ? lhs.values[1] : rhs.values[1],
+        lhs.values[2] > rhs.values[2] ? lhs.values[2] : rhs.values[2]
+    };
+}
+
+inline Vector3neon Min(const Vector3neon& lhs, const Vector3neon& rhs)
+{
+    return Vector3neon
+    {
+        lhs.values[0] < rhs.values[0] ? lhs.values[0] : rhs.values[0],
+        lhs.values[1] < rhs.values[1] ? lhs.values[1] : rhs.values[1],
+        lhs.values[2] < rhs.values[2] ? lhs.values[2] : rhs.values[2]
+    };
+}
+
+}}} // namespace
+
 #endif // VECTOR3NEON_HPP
--- source/State/Implementation/Vector3sse2.hpp
+++ source/State/Implementation/Vector3sse2.hpp
@@ -21,4 +21,404 @@
 #ifndef VECTOR3SSE2_HPP
 #define VECTOR3SSE2_HPP
 
+#include "Vector.hpp"
+
+#include <emmintrin.h>
+
+#include <array>
+#include <cmath>
+#include <limits>
+
+// RAM: TODO: single inputs are called lhs, double, lhs and rhs. Validate.
+
+namespace GameInABox { namespace State { namespace Implementation {
+
+struct alignas(16) Vector3sse2
+{
+    struct tagReplicate {};
+
+    std::array<float, 4> values;
+    //__m128 values;
+
+    constexpr Vector3sse2()
+        : values{{0.0f, 0.0f, 0.0f, 0.0f}} {}
+    constexpr Vector3sse2(float x)
+        : values{{x, 0.0f, 0.0f, 0.0f}} {}
+    constexpr Vector3sse2(float x, float y)
+        : values{{x, y, 0.0f, 0.0f}} {}
+    constexpr Vector3sse2(float x, float y, float z)
+        : values{{x, y, z, 0.0f}} {}
+    constexpr Vector3sse2(float x, float y, float z, float w)
+        : values{{x, y, z, w}} {}
+
+    constexpr Vector3sse2(Vector vector)
+        : values(vector.values) {}
+    constexpr Vector3sse2(const std::array<float, 4>& array)
+        : values(array) {}
+    constexpr Vector3sse2(float x, tagReplicate)
+        : values{{x, x, x, x}} {}
+
+    Vector3sse2(const Vector3sse2&) = default;
+    Vector3sse2(Vector3sse2&&) = default;
+    Vector3sse2& operator=(const Vector3sse2&) & = default;
+    Vector3sse2& operator=(Vector3sse2&&) & = default;
+
+    constexpr Vector ToVector() const { return Vector{values}; }
+};
+
+
+// ///////////////////
+// Save
+// ///////////////////
+
+inline constexpr Vector Save(Vector3sse2 v)
+{
+    return Vector
+    {
+            v.values,
+    };
+}
+
+// ///////////////////
+// Operators
+// ///////////////////
+// Taken from http://stackoverflow.com/questions/4421706/operator-overloading/4421719
+// However all "inclass" operators changed to out of class.
+
+// ///////////////////
+// Increment / Decrement
+// ///////////////////
+inline Vector3sse2& operator++(Vector3sse2& rhs)
+{
+    ++(rhs.values[0]);
+    ++(rhs.values[1]);
+    ++(rhs.values[2]);
+
+    return rhs;
+}
+
+inline Vector3sse2 operator++(Vector3sse2& lhs, int)
+{
+   auto copy = lhs;
+   ++lhs;
+
+   return copy;
+}
+
+inline Vector3sse2& operator--(Vector3sse2& rhs)
+{
+    --(rhs.values[0]);
+    --(rhs.values[1]);
+    --(rhs.values[2]);
+
+    return rhs;
+}
+
+inline Vector3sse2 operator--(Vector3sse2& lhs, int)
+{
+    auto copy = lhs;
+    --lhs;
+
+    return copy;
+}
+
+// ///////////////////
+// Comparison Operators
+// ///////////////////
+inline bool operator==(const Vector3sse2& lhs, const Vector3sse2& rhs)
+{
+    return  (
+                (lhs.values[0]==rhs.values[0]) &&
+                (lhs.values[1]==rhs.values[1]) &&
+                (lhs.values[2]==rhs.values[2])
+            );
+}
+
+inline bool operator!=(const Vector3sse2& lhs, const Vector3sse2& rhs){return  !operator==(lhs,rhs);}
+
+// ///////////////////
+// Prototypes
+// ///////////////////
+inline Vector3sse2 Absolute(const Vector3sse2& lhs);
+
+// ///////////////////
+// Simple Maths
+// ///////////////////
+inline Vector3sse2& operator+=(Vector3sse2& lhs, const Vector3sse2& rhs)
+{
+    lhs.values[0] += rhs.values[0];
+    lhs.values[1] += rhs.values[1];
+    lhs.values[2] += rhs.values[2];
+    return lhs;
+}
+
+inline Vector3sse2& operator-=(Vector3sse2& lhs, const Vector3sse2& rhs)
+{
+    lhs.values[0] -= rhs.values[0];
+    lhs.values[1] -= rhs.values[1];
+    lhs.values[2] -= rhs.values[2];
+    return lhs;
+}
+
+inline Vector3sse2& operator*=(Vector3sse2& lhs, const Vector3sse2& rhs)
+{
+    lhs.values[0] *= rhs.values[0];
+    lhs.values[1] *= rhs.values[1];
+    lhs.values[2] *= rhs.values[2];
+    return lhs;
+}
+
+inline Vector3sse2& operator/=(Vector3sse2& lhs, const Vector3sse2& rhs)
+{
+    lhs.values[0] /= rhs.values[0];
+    lhs.values[1] /= rhs.values[1];
+    lhs.values[2] /= rhs.values[2];
+    return lhs;
+}
+
+inline constexpr Vector3sse2 operator-(const Vector3sse2& lhs)
+{
+    return Vector3sse2
+    {
+        -lhs.values[0],
+        -lhs.values[1],
+        -lhs.values[2],
+        0.0f
+    };
+}
+
+inline Vector3sse2 operator+(Vector3sse2 lhs, const Vector3sse2& rhs){ lhs += rhs;  return lhs; }
+inline Vector3sse2 operator-(Vector3sse2 lhs, const Vector3sse2& rhs){ lhs -= rhs;  return lhs; }
+inline Vector3sse2 operator*(Vector3sse2 lhs, const Vector3sse2& rhs){ lhs *= rhs;  return lhs; }
+inline Vector3sse2 operator/(Vector3sse2 lhs, const Vector3sse2& rhs){ lhs /= rhs;  return lhs; }
+
+inline Vector3sse2& operator*=(Vector3sse2& lhs, float rhs)
+{
+    lhs.values[0] *= rhs;
+    lhs.values[1] *= rhs;
+    lhs.values[2] *= rhs;
+    return lhs;
+}
+
+inline Vector3sse2& operator/=(Vector3sse2& lhs, float rhs)
+{
+    return lhs *= 1.0f / rhs;
+}
+
+inline Vector3sse2 operator*(Vector3sse2 lhs, float rhs){ lhs *= rhs;  return lhs; }
+inline Vector3sse2 operator/(Vector3sse2 lhs, float rhs){ lhs /= rhs;  return lhs; }
+
+// ///////////////////
+// Complicated Maths (single return)
+// ///////////////////
+
+inline float DotF(const Vector3sse2& lhs, const Vector3sse2& rhs)
+{
+    return
+            (lhs.values[0] * rhs.values[0]) +
+            (lhs.values[1] * rhs.values[1]) +
+            (lhs.values[2] * rhs.values[2]) +
+            (lhs.values[3] * rhs.values[3]);
+}
+
+inline float LengthSquaredF(const Vector3sse2& lhs)
+{
+    return DotF(lhs, lhs);
+}
+
+inline float LengthF(const Vector3sse2& lhs)
+{
+    return sqrt(LengthSquaredF(lhs));
+}
+
+inline float DistanceF(const Vector3sse2& lhs, const Vector3sse2& rhs)
+{
+    return LengthF(lhs - rhs);
+}
+
+inline float DistanceSquaredF(const Vector3sse2& lhs, const Vector3sse2& rhs)
+{
+    return LengthSquaredF(lhs - rhs);
+}
+
+// RAM: TODO: Use Radians type.
+inline float Angle(const Vector3sse2& lhs, const Vector3sse2& rhs)
+{
+    auto squaredLengths = LengthSquaredF(lhs) * LengthSquaredF(rhs);
+
+    return acos(DotF(lhs, rhs) / squaredLengths);
+}
+
+// RAM: TODO: What does this do? Used to figure out the determinant of matrixes. Copied from btVector3
+inline float TripleF(const Vector3sse2& lhs, const Vector3sse2& v1, const Vector3sse2& v2)
+{
+    return
+        lhs.values[0] * (v1.values[1] * v2.values[2] - v1.values[2] * v2.values[1]) +
+        lhs.values[1] * (v1.values[2] * v2.values[0] - v1.values[0] * v2.values[2]) +
+        lhs.values[2] * (v1.values[0] * v2.values[1] - v1.values[1] * v2.values[0]);
+}
+
+inline int AxisMax(const Vector3sse2& lhs)
+{
+    return lhs.values[0] < lhs.values[1] ?
+            (lhs.values[1] < lhs.values[2] ? 2 : 1) :
+            (lhs.values[0] < lhs.values[2] ? 2 : 0);
+}
+
+inline int AxisMin(const Vector3sse2& lhs)
+{
+    return lhs.values[0] < lhs.values[1] ?
+            (lhs.values[0] < lhs.values[2] ? 0 : 2) :
+            (lhs.values[1] < lhs.values[2] ? 1 : 2);
+}
+
+inline int AxisFar(const Vector3sse2& lhs)
+{
+    return AxisMax(Absolute(lhs));
+}
+
+inline int AxisNear(const Vector3sse2& lhs)
+{
+    return AxisMin(Absolute(lhs));
+}
+
+inline bool IsZero(const Vector3sse2& lhs)
+{
+    return (lhs.values[0] == 0.0f) &&
+           (lhs.values[1] == 0.0f) &&
+           (lhs.values[2] == 0.0f);
+}
+
+inline bool IsZeroFuzzy(const Vector3sse2& lhs)
+{
+    return LengthSquaredF(lhs) < std::numeric_limits<float>::epsilon();
+}
+
+// ///////////////////
+// Complicated Maths (vector return)
+// ///////////////////
+
+// RAM: TODO: vector versions of all of the above.
+// Need all the single return functions to support vector return too.
+// http://www.gamasutra.com/view/feature/132636/designing_fast_crossplatform_simd_.php?print=1
+// (Keep Results Into SIMD Registers)
+// That is, stop casting between SIMD and Float registers when it can all be kept in SIMD.
+//inline Vector3sse2 LengthSquared(const Vector3sse2& lhs);
+//inline Vector3sse2 Length(const Vector3sse2& lhs);
+//inline Vector3sse2 Distance(const Vector3sse2& lhs, const Vector3sse2& rhs);
+//inline Vector3sse2 DistanceSquared(const Vector3sse2& lhs, const Vector3sse2& rhs);
+//// This too?
+//inline Vector3sse2 Angle(const Vector3sse2& lhs, const Vector3sse2& rhs);
+
+inline Vector3sse2 Dot(const Vector3sse2& lhs, const Vector3sse2& rhs)
+{
+    float dot = DotF(lhs, rhs);
+
+    return Vector3sse2
+    {
+        dot,
+        dot,
+        dot
+    };
+}
+
+inline Vector3sse2 Dot3(const Vector3sse2 &lhs, const Vector3sse2 &v0, const Vector3sse2 &v1, const Vector3sse2 &v2)
+{
+    return Vector3sse2
+    {
+        DotF(lhs, v0),
+        DotF(lhs, v1),
+        DotF(lhs, v2)
+    };
+}
+
+inline Vector3sse2 Absolute(const Vector3sse2& lhs)
+{
+    return Vector3sse2
+    {
+        std::fabs(lhs.values[0]),
+        std::fabs(lhs.values[1]),
+        std::fabs(lhs.values[2]),
+        0.0f
+    };
+}
+
+inline Vector3sse2 NormaliseStable(Vector3sse2 lhs)
+{
+    auto absolute = Absolute(lhs);
+    auto maxIndex = AxisMax(absolute);
+
+    if (absolute.values[maxIndex] > 0)
+    {
+        lhs /= absolute.values[maxIndex];
+        return lhs / LengthF(lhs);
+    }
+
+    // Too small to actually normalise without becoming unstable.
+    // So just set x to 1. Same diff.
+    return Vector3sse2
+    {
+            1.0f
+    };
+}
+
+inline Vector3sse2 Normalise(Vector3sse2 lhs)
+{
+    return lhs / LengthF(lhs);
+}
+
+inline Vector3sse2 Cross(const Vector3sse2& lhs, const Vector3sse2& rhs)
+{
+    return Vector3sse2
+    {
+        lhs.values[1] * rhs.values[2] - lhs.values[2] * rhs.values[1],
+        lhs.values[2] * rhs.values[0] - lhs.values[0] * rhs.values[2],
+        lhs.values[0] * rhs.values[1] - lhs.values[1] * rhs.values[0]
+    };
+}
+
+// RAM: TODO: Use rotation in radians.
+inline Vector3sse2 Rotate(Vector3sse2 lhs, const Vector3sse2& wAxis, float rotation)
+{
+    // Stole this from  bullet3's btVector3
+
+    auto o = wAxis * DotF(wAxis, lhs);
+    auto _x = lhs - o;
+    auto _y = Cross(wAxis, lhs);
+
+    return (o + _x * cosf(rotation) + _y * sinf(rotation));
+}
+
+inline Vector3sse2 Lerp(const Vector3sse2& lhs, const Vector3sse2& rhs, float scale)
+{
+    return Vector3sse2
+    {
+        lhs.values[0] + (rhs.values[0] - lhs.values[0]) * scale,
+        lhs.values[1] + (rhs.values[1] - lhs.values[1]) * scale,
+        lhs.values[2] + (rhs.values[2] - lhs.values[2]) * scale
+    };
+}
+
+inline Vector3sse2 Max(const Vector3sse2& lhs, const Vector3sse2& rhs)
+{
+    return Vector3sse2
+    {
+        lhs.values[0] > rhs.values[0] ? lhs.values[0] : rhs.values[0],
+        lhs.values[1] > rhs.values[1] ? lhs.values[1] : rhs.values[1],
+        lhs.values[2] > rhs.values[2] ? lhs.values[2] : rhs.values[2]
+    };
+}
+
+inline Vector3sse2 Min(const Vector3sse2& lhs, const Vector3sse2& rhs)
+{
+    return Vector3sse2
+    {
+        lhs.values[0] < rhs.values[0] ? lhs.values[0] : rhs.values[0],
+        lhs.values[1] < rhs.values[1] ? lhs.values[1] : rhs.values[1],
+        lhs.values[2] < rhs.values[2] ? lhs.values[2] : rhs.values[2]
+    };
+}
+
+}}} // namespace
+
 #endif // VECTOR3SSE2_HPP
