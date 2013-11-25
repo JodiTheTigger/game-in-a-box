65eab336c13f4c3f89bed4fe4c4b83fd19fff389
Richard Maxwell <jodi.the.tigger@gmail.com>
2013-11-26 07:02:44 +1300

    Finally fleshed out a game loop!

--- source/State/Implementation/Interaction.cpp
+++ source/State/Implementation/Interaction.cpp
@@ -22,6 +22,7 @@
 
 #include <array>
 #include <algorithm>
+#include <tuple>
 
 namespace GameInABox { namespace State { namespace Implementation {
 
@@ -289,10 +290,6 @@ vector<Entity> Process(const vector<Entity>& theWorld, vector<SequenceProcess> p
         Iterate to perfection as opposed to perfecting first shot.
 
 
-        for (loop(world))
-        {
-            newWorld[loop] = Collide(oldWorld[loop], constants);
-        }
 
         // filters (can generalise this, as I was doing before)
         for (loop(world))
@@ -300,9 +297,16 @@ vector<Entity> Process(const vector<Entity>& theWorld, vector<SequenceProcess> p
             if (nothing) nothings.push_back(loop);
             if (willcreate) creators.push_back(loop);
             if (cancollide) colliders.push_back(loop);
+            if (withconstants) colliders.push_back(loop);
         }
 
         // SYNC!
+
+
+        for (loop(colliders))
+        {
+            newWorld[loop] = Collide(oldWorld[loop], constants);
+        }
 
         // create things from nothing (missles, messages)
         // map
@@ -370,6 +374,155 @@ vector<Entity> Process(const vector<Entity>& theWorld, vector<SequenceProcess> p
 
         // sync
     */
+}
+
+// Third try
+enum class ActionType
+{
+    Map,
+    ForEach,
+    FirstOneOnly,
+};
+
+
+struct GameCode
+{
+    using TestSingle      = function<bool(const Entity&)>;
+    using TestPair        = function<bool(const Entity&, const Entity&)>;
+
+    using ActionThink     = function<Entity(Entity, const EntityConstants&)>;
+    using ActionCreate    = function<pair<Entity, Entity>(Entity)>;
+    using ActionMap       = function<Entity(Entity)>;
+    using ActionMapPair   = function<pair<Entity, Entity>(Entity, Entity)>;
+
+    // Tests
+    TestSingle  canThink;
+    TestSingle  canCreate;
+    TestSingle  canCollide;
+    TestSingle  canMove;
+
+    TestPair    collides;
+
+    // Actions
+    ActionThink     think;
+    ActionCreate    create;
+    ActionMapPair   resolve;
+    ActionMap       move;
+};
+
+// /////////////////////
+// NOTE: This is meant to be iterated. Just write it so it works, then generalise it!
+// /////////////////////
+vector<Entity> OneGameTick(const vector<Entity>& oldWorld, GameCode theGame)
+{
+    array<vector<unsigned>, static_cast<int>(EntityType::MaxValue) + 1> filters;
+    auto newWorld = vector<Entity>{};
+    const auto size = oldWorld.size();
+
+    newWorld.reserve(oldWorld.size());
+
+    // Step 1 - Copy the old world to the new world and Think.
+    //          Also get a list of thinkers and nones.
+
+    auto creators = vector<unsigned>{};
+    auto nothings = vector<unsigned>{};
+
+    for (unsigned i = 0; i < size; ++i)
+    {
+        Entity newGuy = oldWorld[i];
+
+        if (theGame.canThink(newGuy))
+        {
+            for (auto constantIndex : filters[static_cast<unsigned>(EntityType::Constants)])
+            {
+                //-gravity to all players
+                //-move/jet/jump to all players
+                //-jet to all missles
+                //-jet energy (can player jet)
+                //-health (is player alive)
+                //-has game finished
+                //-missle timer (can player fire missle)
+                //-fire missles
+                //-exploder missle
+                //-transfer points from/to missle target
+                //-kill player is player health < 0
+                //-slow player speed if on ground (friction)
+                //-slow player speed if in air (drag)
+                //-cap speed if larger than max game speed
+                //-player hitting ground on a fall
+                //-changing type
+                newGuy = theGame.think(newGuy, oldWorld[constantIndex].constants);
+            }
+        }
+
+        if (theGame.canCreate(newGuy))
+        {
+            creators.emplace_back(i);
+        }
+        else
+        {
+            if (newGuy.type == EntityType::None)
+            {
+                nothings.emplace_back(i);
+            }
+        }
+
+        newWorld.emplace_back(newGuy);
+    }
+
+    // oldWorld is no longer referenced from here on.
+
+    // Step 2 - Convert None entities to something else (eg a missle).
+    auto totalCreates = min(
+            nothings.size(),
+            creators.size());
+
+    for (unsigned i = 0; i < totalCreates; ++i)
+    {
+        tie(newWorld[nothings[i]], newWorld[creators[i]]) = theGame.create(newWorld[creators[i]]);
+    }
+
+    // Step 3 - Detect Collisions
+    auto collisions = vector<pair<unsigned, unsigned>>{};
+
+    for (unsigned i = 0; i < size; ++i)
+    {
+        const auto& a = newWorld[i];
+
+        if (theGame.canCollide(a))
+        {
+            for (unsigned j = i + 1; j < size; ++j)
+            {
+                const auto& b = newWorld[j];
+
+                if (theGame.canCollide(b))
+                {
+                    if (theGame.collides(a, b))
+                    {
+                        collisions.emplace_back(i,j);
+                    }
+                }
+            }
+        }
+    }
+
+    // Step 4 - Process Collisions
+    for (const auto& collision : collisions)
+    {
+        tie(newWorld[collision.first], newWorld[collision.second]) =
+                theGame.resolve(newWorld[collision.first], newWorld[collision.second]);
+    }
+
+    // Step 5 - Apply Acceleration to Velocity, Velocity to Position
+    for (auto& entity : newWorld)
+    {
+        if (theGame.canMove(entity))
+        {
+            entity = theGame.move(entity);
+        }
+    }
+
+    return newWorld;
 }
 
 }}} // namespace
